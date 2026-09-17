1d2b2593c4ce5727eef99b6040a80e4012b70554
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-09-17 15:15:24 +0530

    implemented supabase projectID

---
+++ utils/supabase/info.tsx
@@ -0,0 +1,5 @@
+/*
+    This contains supabase project ID and should not be edited!!
+ */
+export const projectId = "ghqrcywxvpqampjywuqs"
+export const publicAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdocXJjeXd4dnBxYW1wanl3dXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5MjM1NzAsImV4cCI6MjEwNDQ5OTU3MH0.EsRff6mb6lrhnPFjDZJ5tkj51W6pnn5TqxheVWCCT9M"
